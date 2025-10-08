ba86ae31b7843ae3ba9e3cc6f8d6bb62fb8b0d4d
Alfonso Casanueva <acasrom2101@iescarrillo.es>
2025-10-08 11:17:20 +0200

    Actualización 1.0.0

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { login } from "./feature/login";
 import { mostrarMesasDisponibles } from "./feature/table-list";
 console.log("===== SISTEMA DE RESERVAS v1.0.0 =====");
-const acceso = login("ana@example.com", "1234");
+const acceso = login("test@dummy.com", "12345");
 if (acceso) {
-console.log("Acceso concedido ");
+console.log("Acceso exitoso");
 mostrarMesasDisponibles();
 } else {
-console.log("Acceso denegado ");
+console.log("Usuario no reconocido ");
 }
