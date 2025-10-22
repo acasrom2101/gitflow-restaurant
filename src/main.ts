fd121f376c9f263567b416b73b55f1ca1bc9a49d
Alfonso Casanueva <acasrom2101@iescarrillo.es>
2025-10-22 12:30:34 +0200

    Añadida funcionalidad para recompensas de usuario

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ if (acceso) {
 } else {
     console.log("Usuario no reconocido ");
 }
+export function procesarReserva(cliente: string, total: number): string {
+    console.log(`Procesando reserva para ${cliente}.`);
+    console.log(`Total a pagar: ${total} €`);
+    let puntosGanados = Math.floor(total / 10);
+    console.log(` El cliente ${cliente} ha ganado ${puntosGanados} puntos.`);
+    return "Reserva completada correctamente con puntos de recompensa.";
+}
